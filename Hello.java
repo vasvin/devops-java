835b0f3c11525751472aec8735d04b556d363c45
Vinay <vinay.vasisht01@gmail.com>
2025-07-03 23:20:47 +0530

    Hello.java modified

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Hello{
-  public static void main9String[] args) {
-    System.out.println("Hello World in the tutorial");
+  public static void main(String[] args) {
+    System.out.println("Hello World");
   }
 }
